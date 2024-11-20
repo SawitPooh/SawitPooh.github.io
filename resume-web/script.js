f76f9a97abd8e3b9aac978ae99cf7441c0140a40
SawitPooh <119642304+SawitPooh@users.noreply.github.com>
2024-11-20 23:24:10 +0700

    Resume
    Resume

---
+++ resume-web/script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault(); 
+        const targetId = this.getAttribute('href').substring(1); 
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
